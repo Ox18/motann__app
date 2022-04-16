bc9caaecc0cbd5f0445b0816b9b20cfbbbcb3b6f
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-04-16 17:53:30 -0500

    change frank

--- pages/api/clientes/[pid].js
+++ pages/api/clientes/[pid].js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
         const { pid } = req.query;
         await db.collection("clientes").doc(pid).delete();
         res.status(200).json({
-            message: "Cliente deletado com sucesso",
+            message: "Cliente deletado com sucess",
         });
     }
 }
